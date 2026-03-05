6f89bb2a255ac7e1e9856dfbdc56806557ada69e
samwhelp <samwhelp@users.noreply.github.com>
2026-03-05 13:46:18 +0800

    fix

--- make-cinnamon-style.sh
+++ make-cinnamon-style.sh
@@ -1669,6 +1669,29 @@ sys_style_config_install_for_orchis () {
 dconf load / << __EOF__
 
 
+[org/cinnamon/desktop/interface]
+gtk-theme='Orchis-Dark'
+icon-theme='Citrus-red-dark'
+cursor-theme='Vimix-cursors'
+cursor-size=24
+
+
+[org/cinnamon/desktop/wm/preferences]
+theme='Orchis-Dark'
+
+
+[org/cinnamon/theme]
+name='Orchis-Dark'
+
+
+__EOF__
+
+
+
+
+dconf load / << __EOF__
+
+
 [org/gnome/desktop/interface]
 color-scheme='prefer-dark'
 gtk-theme='Orchis-Dark'
